a12049a1aa21c580ba75f09b1fc92fa7ec42094d
KyzukY <kyzuk.yr@gmail.com>
2022-02-01 13:56:48 +0200

    homework

---
+++ js/Lesson1/Homework/main.js
@@ -0,0 +1,100 @@
+let text = 'hello';
+let secondText = 'owu';
+let trirdText = 'com';
+let fourthText = 'ua';
+let firstNumber = 1;
+let tenNumber = 10;
+let negativeNumber = -999;
+let ordinalNumber = 123;
+let pi = 3.14;
+let floatNumbers = 2.7;
+let numbers = 16;
+let a = true;
+let b = false;
+
+console.log(a, b, text, secondText, trirdText, fourthText, firstNumber, tenNumber, negativeNumber,
+    ordinalNumber,pi,floatNumbers,numbers)
+// alert(text)
+// alert(secondText)
+// alert(trirdText)
+// alert(fourthText)
+// alert(firstNumber)
+// alert(tenNumber)
+// alert(negativeNumber)
+// alert(ordinalNumber)
+// alert(pi)
+// alert(floatNumbers)
+// alert(numbers)
+// alert(a)
+// alert(b)
+document.write([text,secondText,trirdText,fourthText,firstNumber,tenNumber,negativeNumber,
+ordinalNumber,pi,floatNumbers,numbers,a,b])
+text = 'world';
+secondText = 'okten';
+trirdText = 'gov';
+fourthText = 'at';
+firstNumber = 3;
+tenNumber = 1000;
+negativeNumber = -500;
+ordinalNumber = 789;
+pi = 3.141592;
+floatNumbers = 6.5;
+numbers = 50;
+a = false;
+b = true;
+console.log(a, b, text, secondText, trirdText, fourthText, firstNumber, tenNumber, negativeNumber,
+    ordinalNumber,pi,floatNumbers,numbers)
+// alert(text)
+// alert(secondText)
+// alert(trirdText)
+// alert(fourthText)
+// alert(firstNumber)
+// alert(tenNumber)
+// alert(negativeNumber)
+// alert(ordinalNumber)
+// alert(pi)
+// alert(floatNumbers)
+// alert(numbers)
+// alert(a)
+// alert(b)
+document.write([text,secondText,trirdText,fourthText,firstNumber,tenNumber,negativeNumber,
+    ordinalNumber,pi,floatNumbers,numbers,a,b])
+let firstName='Юрій';
+let middleName='Любомирович';
+let lastName='Кузик';
+let age=31;
+
+let person=[firstName,middleName,lastName,age]
+
+// prompt('Введіть Імя' )
+// prompt('Введіть По-батькові' )
+// prompt('Введіть Вік')
+// alert('Вітаю ' + firstName +' '+ middleName + '.Тобі ' + age + ' років .')
+
+let aa=100
+console.log(typeof aa)
+let bb='100'
+console.log(typeof bb)
+let c=true
+console.log(typeof c)
+
+console.log(5!==6)
+console.log(5>6)
+console.log(5===6)
+console.log(5>=6)
+
+console.log(10>=10)
+console.log(10==10)
+console.log(10>10)
+console.log(10!==10)
+console.log(10<10)
+
+console.log(123==='123')
+console.log(123 =='123')
+
+// let str = "20";
+// let d = 5;
+// document.write(str + d + "<br/>"); =205,тому що str перетворило d в стрічку,конкантенація.)
+// document.write(str - d + "<br/>");=15;
+// document.write(str * "2" + "<br/>");=40;
+// document.write(str / 2 + "<br/>");=10;
