80da06fc0a1b045b68c522069e589a6358a6639b
KyzukY <kyzuk.yr@gmail.com>
2022-02-06 15:48:50 +0200

    classwork 3.

---
+++ js/lesson3/classwork/cw3.js
@@ -0,0 +1,122 @@
+let arr=[2,17,13,6,22,31,45,66,100,-18]
+// 1. перебрати його циклом while
+
+// let i=0
+// while (i<arr.length){
+//     console.log(arr[i])
+//     i++
+// }
+
+// 2. перебрати його циклом for
+
+// for (let j = 0; j < arr.length; j++) {
+//     let arrElement = arr[j];
+//     console.log(arrElement)
+// }
+
+// 3. перебрати циклом while та вивести  числа тільки з непарним індексом
+
+// let i=0
+// while (i<arr.length){
+//     if (i%2===0){
+//         console.log(arr[i])
+//     }
+//     i++
+// }
+
+// перебрати циклом for та вивести  числа тільки з непарним індексом
+//
+// for (let i = 0; i < arr.length; i++) {
+//     let arrElement = arr[i];
+//     if (i%2===0){
+//         console.log(arrElement)
+//     }
+// }
+
+// перебрати циклом while та вивести  числа тільки парні  значення
+
+// let i=0
+// while (i<arr.length){
+//     if(i%2===1){
+//         console.log(arr[i])
+//     }
+//     i++
+// }
+
+// перебрати циклом for та вивести  числа тільки парні  значення
+
+// for (let i = 0; i < arr.length; i++) {
+//     const arrElement = arr[i];
+//     if(i%2!==0){
+//         console.log(arrElement)
+//     }
+// }
+
+// замінити кожне число кратне 3 на слово "okten"
+
+// for (let i = 0; i < arr.length; i++) {
+//     let arrElement = arr[i];
+//     if(arrElement%3===0){
+//         arrElement=`okten`
+//         console.log(arrElement)
+//     }
+// }
+
+// вивести масив в зворотньому порядку.
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     const arrElement = arr[i];
+//     console.log(arrElement)
+// }
+
+// всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
+
+// let i=0
+// while (i<arr.length){
+//     console.log(arr[i])
+//     i--
+// }
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     const arrElement = arr[i];
+//     console.log(arrElement)
+// }
+
+// let i=arr.length-1
+// while (i>=0){
+//     if (i%2===0){
+//         console.log(arr[i])
+//     }
+//     i--
+// }
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     let arrElement = arr[i];
+//     if (arrElement%2===0){
+//         console.log(arrElement)
+//     }
+// }
+
+// let i=arr.length-1
+// while (i>=0){
+//     if(i%2!==0){
+//         console.log(arr[i])
+//     }
+//     i--
+// }
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     let arrElement = arr[i];
+//     if(i%2 !==0){
+//         console.log(arrElement)
+//     }
+// }
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     let arrElement = arr[i];
+//     if(arrElement%3===0){
+//         arrElement="okten"
+//         console.log(arrElement)
+//     }
+// }
+
