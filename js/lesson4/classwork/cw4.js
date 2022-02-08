d42120435b2d22bff3f644d3a1984c484ec8d098
KyzukY <kyzuk.yr@gmail.com>
2022-02-08 16:54:43 +0200

    homework 4.

--- js/lesson4/classwork/cw4.js
+++ js/lesson4/classwork/cw4.js
@@ -33,14 +33,17 @@
 
 // створити функцію яка повертає найбільше число з масиву
 
-// let array=[54,45,87,12,56,98,232,6,34]
-// function max(arr){
-//     for (let arrElement of arr) {
-//
-//     }
-// }
-//
-// max(array)
+let array=[54,45,87,12,56,98,232,6,34]
+function max(arr){
+    for (let arrElement of arr) {
+        if(arrElement>arrElement[0]){
+            console.log(arrElement)
+        }
+    }
+    return
+}
+
+max(array)
 
 // створити функцію яка повертає найменьше число з масиву
 
@@ -52,11 +55,38 @@
 
 // створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
 
-let array=[54,45,87,12,56,98,232,6,34]
-function total(arr){
-    let sum=0
-    sum=sum+array[0]
-    return sum;
-}
+// let array=[54,45,87,12,56,98,232,6,34]
+// function total(arr){
+//     let sum=0
+//     sum=sum+array[0]
+//     return sum;
+// }
+//
+// total(array)
 
-total(array)
+// створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+
+
+// створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
+
+
+// створити функцію яка заповнює масив рандомними числами Math.round(Math.random()*100))
+
+function random() {
+    let arrRandom=[];
+}
+console.log(random())
+
+// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+
+function random(limit) {
+    let arrRandom=[];
+}
+console.log(random())
+
+// Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+
+function reverseArr() {
+    let arr=[]
+    for(i>=0,arr.length-1,i--){}
+}
