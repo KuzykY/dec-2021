fc24e9b18bfdf3f05611feb25d96a336f8738cec
KyzukY <kyzuk.yr@gmail.com>
2022-02-20 15:35:04 +0200

    classwork 8

---
+++ js/Lesson8/classwork/cw8.js
@@ -0,0 +1,64 @@
+// a) додає клас з назвою групи, елементу з ід main_header
+
+// let header=document.getElementById('main_header');
+// header.classList.add('dec-2021');
+
+// b) робить шириниу елементу ul 400px
+
+// let widhtUl=document.getElementsByTagName("ul");
+// widhtUl[0].style.width='400px';
+
+// c) робить шириниу всіх елементів з класом linkList шириною 50%
+
+// let changeWidth=document.getElementsByClassName('linkList');
+// for (let changeWidthElement of changeWidth) {
+//     changeWidthElement.style.width='50%'
+// }
+
+// отримує текст який зберігається в елементі з класом listElement2
+
+// let textLi=document.getElementsByClassName('listElement2');
+// textLi[0].innerHTML='<a href="">new text</a>';
+
+// отримує всі елементи li та змінює ім колір фону на сірий
+
+// let changeLi=document.getElementsByTagName('li');
+// for (let changeLiElement of changeLi) {
+//     changeLiElement.style.background='grey'
+// }
+
+// отримує всі елементи 'a' та додає їм клас anchor
+
+// let elementA=document.getElementsByTagName('a');
+// for (let elementAElement of elementA) {
+//     elementAElement.classList.add('anchor')
+//
+// }
+
+// g) отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
+
+// let elemetnA=document.getElementsByTagName('a');
+// for (let elemetnAElement of elemetnA) {
+//     if(elemetnAElement.innerText==='link3'){
+//         elemetnAElement.style.fontSize=`40px`
+//     }
+// }
+
+// отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
+
+// let elementA=document.getElementsByTagName('a');
+// for (let elementAElement of elementA) {
+//     let xxx=elementAElement.innerText;
+//     elementAElement.classList.add(`element_${xxx}`)
+// }
+
+// отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
+//  let changeBg=document.getElementsByClassName('sub-header');
+// for (let changeBgElement of changeBg) {
+//     changeBgElement.style.background=prompt("Ввeдіть колір:","red",'blue','yellow')
+//     if(typeof prompt!=='string')
+//         alert("Ви ввели не правильний колір")
+// }
+
+// отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
+
