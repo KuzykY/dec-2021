7740d8d46eaeaf2a8ec15f8cb146b45e27d59f23
KyzukY <kyzuk.yr@gmail.com>
2022-02-20 14:39:21 +0200

    homework 8

---
+++ js/Lesson8/homework/hw8.js
@@ -0,0 +1,38 @@
+// Напишіть код,  котрий :
+// -- отримує текст з параграфа з id "content"
+// console.log(document.getElementById('content'))
+
+// отримує текст з блоку з id "rules"
+// console.log(document.getElementById('rules'))
+
+// замініть текст параграфа з id 'content' на будь-який інший
+// let changeP=document.getElementById('content');
+// changeP.innerText='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, perspiciatis.'
+
+// замініть текст параграфа з id 'rules' на будь-який інший
+
+// let changeP=document.getElementById('rules');
+// changeP.innerText='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, quod?'
+
+
+// змініть кожному елементу колір фону на червоний
+// let element=document.body.children
+// for (let elementElement of element) {
+//     elementElement.style.background='red'
+// }
+
+// змініть кожному елементу колір тексту на синій
+
+// let colorText=document.body.children
+// for (let colorTextElement of colorText) {
+//     colorTextElement.style.color='blue'
+// }
+
+// отримати весь список класів елемента з id=rules і вивести їх в console.log
+
+// поміняти колір тексту у всіх елементів fc_rules на червоний
+
+// let colorText=document.getElementsByClassName('fc_rules');
+// for (let colorTextElement of colorText) {
+//     colorTextElement.style.color='red';
+// }
